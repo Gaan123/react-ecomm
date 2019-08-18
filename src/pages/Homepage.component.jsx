71b917de0a787c8c6291b340be9960d1d6be6a08
gagan gurung <gurung.smart@live.com>
2019-08-18 21:58:41 +0545

    Home component\

---
+++ src/pages/Homepage.component.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './homepage.styles.scss';
+import Directory from "../components/directory/Directory";
+class HomepageComponent extends Component {
+    render() {
+        return (
+            <div className="homepage">
+                <Directory/>
+            </div>
+        );
+    }
+}
+
+export default HomepageComponent;
